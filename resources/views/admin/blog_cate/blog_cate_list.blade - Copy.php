90b3b059bdf4f8081899dcb30965978e50992841
Hi-TechParks <softtechhabib@gmail.com>
2022-07-22 14:35:22 +0600

    Backup

--- resources/views/admin/blog_cate/blog_cate_list.blade - Copy.php
+++ resources/views/admin/blog_cate/blog_cate_list.blade - Copy.php
@@ -48,7 +48,7 @@
                                                         <div class="form-group row">
                                                             <label class="col-sm-4 col-form-label">Category Title</label>
                                                             <div class="col-sm-8">
-                                                                <input type="text" class="form-control" name="title" placeholder="Category Title"  value="{{ old('title') }}">
+                                                                <input type="text" class="form-control" name="title" placeholder="Category Title"  value="{{ old('title') }}" required>
 
                                                                 @if ($errors->has('title'))
                                                                     <span class="text-danger">
@@ -111,14 +111,6 @@
                                                     </td>
                                                     <td>
                                                         <a href="/admin/blog/category/edit/{{ $blog_category->id }}" class="btn btn-primary">Edit</a>
-
-
-                                                        <!-- Button trigger modal -->
-                                                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">Add</button>
-
-                                        
-
-
                                                         <a href="/admin/blog/category/delete/{{ $blog_category->id }}" class="btn btn-danger">Delete</a>
                                                     </td>
                                                 </tr>
@@ -133,59 +125,4 @@
                         <!-- end row-->
                         
                     </div> <!-- container -->
-
-
-
-                    <!-- sample modal content -->
-                    <div id="editModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg">
-                            <form class="form-horizontal" action="/admin/blog/category/update/{{ $blog_category->id }}" method="post">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title" id="myModalLabel">Add Blog Category</h4>
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                                </div>
-                                <div class="modal-body">
-                                    
-                                    @csrf
-
-                                    <div class="form-group row">
-                                        <label class="col-sm-4 col-form-label">Category Title</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" name="title" placeholder="Category Title"  value="{{ old('title') }}" required>
-
-                                            @if ($errors->has('title'))
-                                                <span class="text-danger">
-                                                    <strong>{{ $errors->first('title') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group row">
-                                        <label class="col-sm-4 col-form-label">Category Details</label>
-                                        <div class="col-sm-8">
-                                            <textarea class="form-control" name="description" rows="8">{{ old('description') }}</textarea>
-
-                                            @if ($errors->has('description'))
-                                                <span class="text-danger">
-                                                    <strong>{{ $errors->first('description') }}</strong>
-                                                </span>
-                                            @endif
-                                        </div>
-                                    </div>
-        
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
-                                </div>
-                            </div><!-- /.modal-content -->
-                            </form>
-
-                        </div><!-- /.modal-dialog -->
-                    </div><!-- /.modal -->
                     @endsection
-
-
-
